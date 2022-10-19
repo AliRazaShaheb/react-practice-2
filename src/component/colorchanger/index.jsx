c21a2a6c4f837707ccd93143afbd52c397837375
ALI RAZA SHAHEB <89347148+AliRazaShaheb@users.noreply.github.com>
2022-10-19 13:00:50 +0530

    color picker added

--- src/component/colorchanger/index.jsx
+++ src/component/colorchanger/index.jsx
@@ -22,6 +22,7 @@ export const BgContainer = () => {
   const [uiColor, setUiColor] = useState("");
   const changeColor = (color) => {
     setUiColor(color);
+    return color;
   };
   return (
     <div>
@@ -40,20 +41,39 @@ export const BgContainer = () => {
 
 export const ColorInput = ({ changeColor }) => {
   const [input, setInput] = useState("");
+  const [selectColor, setSelectColor] = useState("");
 
   const colorHandler = (e) => {
     const { value } = e.target;
     setInput(value);
+    const latestColor = changeColor(value);
+    setSelectColor(latestColor);
+  };
+  const selectColorHandler = (e) => {
+    const { value } = e.target;
+    setSelectColor(value);
     changeColor(value);
   };
   const randomColor = () => {
     const randColor = `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
-    changeColor(randColor);
+    const latestColor = changeColor(randColor);
+    console.log(latestColor);
+    setSelectColor(latestColor);
   };
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <input type="text" value={input} onChange={colorHandler} />
+      <p>OR</p>
+      <label>
+        Select Color:
+        <input
+          type="color"
+          value={selectColor}
+          onChange={selectColorHandler}
+          style={{ marginLeft: "2rem" }}
+        />
+      </label>
       <p>OR</p>
       <button onClick={randomColor}>Random Color</button>
     </div>
